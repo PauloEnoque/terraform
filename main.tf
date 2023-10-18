04096f2c7ad1ac09224faf190e99ea07b617aa3c
PauloEnoque <pauloenoque2014@gmail.com>
2023-10-18 09:38:08 +0200

    Adding user data script to run a web server

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "example" {
   ami = "ami-0fb653ca2d3203ac1"
   instance_type = "t3.micro"
 
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.xhtml
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
   tags = {
     name = "terraform-example"
   }
