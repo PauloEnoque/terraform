2d143510b4e10fd0bacc6b742c7993303d75db77
PauloEnoque <pauloenoque2014@gmail.com>
2023-10-19 13:38:04 +0200

    ASG Configuration

--- main.tf
+++ main.tf
@@ -13,21 +13,34 @@ output "public_ip" {
   description = "The public IP address of the web server"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0fb653ca2d3203ac1"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0fb653ca2d3203ac1"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [ aws_security_group.instance.id ]
+  security_groups = [aws_security_group.instance.id ]
 
   user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.xhtml
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
-                
-    user_data_replace_on_change = true
+  
+  # To create instances before destroying the old ones, usefill for ASG
+  lifecycle {
+    create_before_destroy = true
+  }
+  
+}
 
-  tags = {
-    name = "terraform-example"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "Terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
