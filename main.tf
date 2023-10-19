2029e5c157ddfb0e7d14489126403cbfcc5212e8
PauloEnoque <pauloenoque2014@gmail.com>
2023-10-19 14:22:15 +0200

    Added ALB

--- main.tf
+++ main.tf
@@ -2,15 +2,26 @@ provider "aws" {
   region = "us-east-2"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [ data.aws_vpc.default.id ]
+  }
+}
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type = number
   default = 8080
 }
 
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
 
 resource "aws_launch_configuration" "example" {
@@ -20,8 +31,8 @@ resource "aws_launch_configuration" "example" {
 
   user_data = <<-EOF
                 #!/bin/bash
-                echo "Hello, World" > index.xhtml
-                nohup busybox httpd -f -p ${var.server_port} &
+                sudo apt-get update
+                sudo apt-get install apache2
                 EOF
   
   # To create instances before destroying the old ones, usefill for ASG
@@ -33,6 +44,10 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  target_group_arns = [ aws_lb_target_group.asg.arn ]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -41,6 +56,83 @@ resource "aws_autoscaling_group" "example" {
     key = "Name"
     value = "Terraform-asg-example"
     propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "example" {
+  name = "terraform-alb-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnets.default.ids
+  security_groups = [ aws_security_group.alb.id ]
+}
+
+resource "aws_lb_listener" "HTTP" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: Page not found"
+      status_code = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.HTTP.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = [ "*" ]
+    }
+  }
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+
+}
+
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+
+resource "aws_security_group" "alb" {
+  name = "terraform-exampl-alb"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
 
